5fa13629cb6e94433df79535e998ac9ca901f270
rhyusarmiento <rhyu.sar.15@gmail.com>
2023-08-29 12:15:50 -0600

    added schedule

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Libary from './library/library';
-// import Schedule from './schedule/schedule';
+import Schedule from './schedule/schedule';
 
 class App extends Component {
   render() {
     return (
       <div className="home">
         <Libary />
+        <Schedule />
       </div>
     );
   }
